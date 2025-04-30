cf851cde6ae9b794eb4aaea82a0bb0b0abc25db9
Guilherme Jr <falecom@guilhermejr.net>
2025-04-30 16:52:01 -0300

    feat: atualizado configuração de segurança

--- src/main/java/net/guilhermejr/sistema/remedioservice/config/security/AuthenticationJwtFilter.java
+++ src/main/java/net/guilhermejr/sistema/remedioservice/config/security/AuthenticationJwtFilter.java
@@ -10,6 +10,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -17,6 +18,7 @@ import java.io.IOException;
 import java.util.UUID;
 
 @Log4j2
+@Component
 public class AuthenticationJwtFilter extends OncePerRequestFilter {
 
     @Autowired
